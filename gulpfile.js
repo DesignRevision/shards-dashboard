a0158477ccca0500582f8dd9348dd8e1468ebbac
Catalin Vasile <contact@catalin.me>
2018-08-23 11:08:17 +0300

    Bump version number (1.0.1)

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ gulp.task('compile:sass', function () {
   return gulp.src('styles/scss/shards-dashboards.scss')
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(rename({ suffix: '.' + require('./package.json').version}))
+    .pipe(gulp.dest('./styles'))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min'}))
     .pipe(gulp.dest('./styles'))
